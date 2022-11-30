1c2bc366961a8c5f636a6b9d174481345217f4f4
DESKTOP-IJULNUP\ifnub <ifnubima@gmail.com>
2022-11-30 22:22:48 +0700

    remove jdbc and spring data jdbc code

---
+++ mosewa-web/src/main/java/id/mosewa/tracking/MosewaTrackingApplication.java
@@ -0,0 +1,15 @@
+package id.mosewa.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class MosewaTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosewaTrackingApplication.class, args);
+    }
+    
+}
---
+++ mosewa-web/src/test/java/id/mosewa/tracking/MosewaTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.mosewa.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosewaTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
